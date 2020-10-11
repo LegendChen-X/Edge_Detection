0500a4a00f42a0fc1d5b0ddc2f88ab96ec77aa46
Xiang <chenxiang00825@gmail.com>
2020-10-11 12:35:09 -0400

    comment

--- q4_code.py
+++ q4_code.py
@@ -6,32 +6,40 @@ from PIL import Image
 def Gaussian_Model(sigma,point):
     x, y = point
     ratio = 1/(2*math.pi*sigma**2)
+# Slightly different for the expression from the lecture. #
     e_part = math.exp(-(x**2+y**2)/(2*sigma**2))
     return ratio * e_part
     
 def Gaussian_Blur(sigma,kernel_size):
+# This can use array to boost access speed. #
     half_kernel = (kernel_size-1) // 2
-    matrix = []
     Gaussian_matrix = []
     for i in range(kernel_size):
-        matrix.append([])
         Gaussian_matrix.append([])
         for j in range(kernel_size):
-            matrix[i].append((j-half_kernel,half_kernel-i))
-            Gaussian_matrix[i].append(Gaussian_Model(sigma,matrix[i][j]))
-    print(Gaussian_matrix)
+            Gaussian_matrix[i].append(Gaussian_Model(sigma,(j-half_kernel,half_kernel-i)))
+# Visualize the Matrix. #
+    for i in range(len(Gaussian_matrix)): print(Gaussian_matrix[i],'\n')
+    print("\n")
     return Gaussian_matrix
     
 def convolution(matrix,x,y,src):
+# This function is only for grey scale image. #
+# Please use getGreyImge first, if input is a RGB image. #
+# Can be optimized by the product of two vectors. #
     x_length, y_length = src.shape
     kernel_size = len(matrix)
+# Help to track the row in Matrix. #
     count_u = 0
     res = 0
+# Get the start and end of k. #
     start = int(-(kernel_size-1)/2)
     end = int((kernel_size-1)/2 + 1)
     for u in range(start,end):
+# Help to track the coloum in Matrix. #
         count_v = 0
         for v in range(start,end):
+# Boundary check. Smarter than padding the image. #
             if(x-u<0 or y-v<0 or x-u>=x_length or y-v>=y_length): res += 0
             else: res += src[x-u][y-v] * matrix[count_u][count_v]
             count_v += 1
@@ -39,6 +47,8 @@ def convolution(matrix,x,y,src):
     return res
     
 def Sobel_Operation(src):
+# This function is only for grey scale image. #
+# Please use getGreyImge first, if input is a RGB image. #
     sobel_x = [[-1,0,1],[-2,0,2],[-1,0,1]]
     sobel_y = [[-1,-2,-1],[0,0,0],[1,2,1]]
     x_length, y_length = src.shape
@@ -67,6 +77,7 @@ def Threshold_Algorithm(gradients):
         M_L = sum(lower_bound) / len(lower_bound)
         M_H = sum(upper_bound) / len(upper_bound)
         tau_1 = (M_L+M_H) / 2
+# Set epsilon to 0.0000001 (enough small)#
         if(abs(tau_0-tau_1)<=0.0000001): break
         tau_0 = tau_1
     edge_map = np.empty((h,w), dtype=float)
@@ -77,21 +88,31 @@ def Threshold_Algorithm(gradients):
     return edge_map
     
 def getGreyImge(img):
+# Change the rgb value to grey. #
     rgb_weights = [0.2989, 0.5870, 0.1140]
     return np.dot(img[...,:3], rgb_weights)
     
 def getEdgeImage(img,Gaussian_Matrix):
+# This function is only for grey scale image. #
+# Please use getGreyImge first, if input is a RGB image. #
     x, y = img.shape
+# Do the filter first to decrease the noise. #
     Gaussian_image = np.empty((x,y), dtype=float)
     for i in range(x):
         for j in range(y):
             Gaussian_image[i][j] = convolution(Gaussian_Matrix,i,j,img)
+# Get the gradient image (array). #
     gradients = Sobel_Operation(Gaussian_image)
     return Threshold_Algorithm(gradients)
     
 if __name__ == '__main__':
-    Gaussian_Matrix = Gaussian_Blur(1.5,5)
+# Get two Gaussian_Matrix. #
+    Gaussian_Matrix = Gaussian_Blur(1.5,3)
+    Gaussian_Matrix_2 = Gaussian_Blur(0.5,5)
+# Get image array. #
     img = plt.imread("./Q4_image_1.jpg")
+# Change to grey scale.#
+# If the image is grey, skip this step. #
     grayscale_image = getGreyImge(img)
     edge_map = getEdgeImage(grayscale_image,Gaussian_Matrix)
     img_g = Image.fromarray(edge_map)
